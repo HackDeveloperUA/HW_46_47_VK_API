38fe0a89a44c855d7bc0a14e9181d1e268671612
MD <md@MacBook-Air-MD.Dlink>
2015-09-10 09:14:06 +0300

    Initial Commit

---
+++ HW_46_47/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_46_47
+//
+//  Created by MD on 10.09.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HW_46_47/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW_46_47
+//
+//  Created by MD on 10.09.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
